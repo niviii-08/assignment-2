e52672c5d362d651aa06308ed71d4309abb34be3
niviii-08 <neevetha2006@gmail.com>
2025-06-08 19:53:56 +0530

    after changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,10 @@ export default function Home() {
   return (
     <div>
       <main>
+        <Link href="/contact">CONTACT</Link>
+        <br />
+        <Link href="/about">ABOUT</Link>
       </main>
-      <footer>
-      </footer>
-    </div>
-  );
+    </div>
+  );
 }
