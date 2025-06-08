02e22259fe63b9b6223ee2329a9e12fc9895a4ae
niviii-08 <neevetha2006@gmail.com>
2025-06-08 19:43:47 +0530

    initial commit

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div>
+      <main>
+        <h2>Welcome to my page! This page is all about how you chat with the AI to get information or describe about the image you have uploaded </h2>
+      </main>
+      <footer>
+      </footer>
+    </div>
+  )
+}
